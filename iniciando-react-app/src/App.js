935b237b59b745dafd4694da6c7e2b50128dd122
Lucas Ribeiro <lucasvsribeiro1@gmail.com>
2020-02-22 19:22:44 -0300

    Aula 11

--- iniciando-react-app/src/App.js
+++ iniciando-react-app/src/App.js
@@ -4,13 +4,32 @@ class App extends React.Component {
 
   state = {
 
-    nome: 'Lucas Ribeiro'
+    nome: ''
+
+  }
+
+  modificarNome = (event) => {
+
+    let nome = event.target.value; //target = o componente, neste caso vai ser o input || value = o valor que o componente está guardando, no caso o que o usuário digitou.
+
+    this.setState({ // Não é possível alterar diretamente a variavel state, tem que ser por este método.
+
+      nome
+
+    })
 
   }
   render(){
 
     return(
-      <h1>Hello {this.state.nome}</h1>
+
+      <>
+
+        <input type = "text" value = {this.state.nome} onChange = { this.modificarNome } />
+        <h1>Hello {this.state.nome}</h1>
+
+      </>
+
     )
 
   }
