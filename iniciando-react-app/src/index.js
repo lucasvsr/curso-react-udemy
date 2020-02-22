018ef3630d5c913dd88f5d537c5de9f361be661c
Lucas Ribeiro <lucasvsribeiro1@gmail.com>
2020-02-22 19:43:51 -0300

    Aula 13

--- iniciando-react-app/src/index.js
+++ iniciando-react-app/src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App nome = "Lucas" />, document.getElementById('root')); // O atributo "nome" é o que chamamos de 'props' e seria uma variavel global do framework, podendo ser acessada de qualquer componente react.
