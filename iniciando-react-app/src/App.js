c5300d3dba17cafba3200acdf264809cd7701ff8
Lucas Ribeiro <lucasvsribeiro1@gmail.com>
2020-02-22 19:10:06 -0300

    Aula 10

--- iniciando-react-app/src/App.js
+++ iniciando-react-app/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+
+    nome: 'Lucas Ribeiro'
+
+  }
+  render(){
+
+    return(
+      <h1>Hello {this.state.nome}</h1>
+    )
+
+  }
+
 }
 
 export default App;
